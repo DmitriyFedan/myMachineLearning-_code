b28c787a1dfaf48f0927cbb245100e2ea3a13cd0
DmitriyFedan <dmitry061093@mail.ru>
2023-11-13 00:31:10 +0700

    createQlearningDotnetProject

---
+++ testQlearning/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using testQlearning.Services;
+
+namespace testQlearning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Trader _trader;
+        decimal _price = 1000;
+        Random _randomvalue;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            _randomvalue = new Random();
+
+            _trader = new Trader(10, "xauusd", 1000, 10);
+
+
+
+
+        }
+
+        private void Button_Click_Buy(object sender, RoutedEventArgs e)
+        {
+            _trader.OrderBy(_price);
+
+            _price = _price + _randomvalue.Next(-20, 20);
+
+        }
+
+        private void Button_Click_Sell(object sender, RoutedEventArgs e)
+        {
+            _trader.OrderSell(_price);
+
+            _price = _price + _randomvalue.Next(-20, 20);
+
+        }
+    }
+}
---
+++ testQlearning/Services/Trader.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace testQlearning.Services
+{
+    public class Trader
+    {   
+        private int _id;
+        private string _symbol;
+        private decimal _balance;
+        private List<Order> _orders;
+        private readonly int _maxOrderCounts;
+
+
+        public Trader(int id, string symbol, decimal balance, int maxOrderCounts)
+        {
+            _id = id;
+            _symbol = symbol;
+            _balance = balance;
+            _orders = new List<Order>();
+            _maxOrderCounts = maxOrderCounts;
+        }
+
+        public void OrderBy(decimal price)
+        {
+            if (_orders.Count >= _maxOrderCounts) 
+            {
+                return;
+            }
+
+            Order newOrder = new Order(_symbol, price, OrderType.BUY);
+            _orders.Add(newOrder);
+
+            Debug.Print($"oreder {newOrder.Type} was opened {newOrder.Id}");
+        }
+
+        public void OrderSell(decimal price)
+        {
+            if (_orders.Count >= _maxOrderCounts)
+            {
+                return;
+            }
+
+            Order newOrder = new Order(_symbol, price, OrderType.SELL);
+            _orders.Add(newOrder);
+
+            Debug.Print($"oreder {newOrder.Type} was opened {newOrder.Id}");
+        }
+
+        public void CloseOrder (Guid id )
+        {
+            var removableOrder = _orders.FirstOrDefault(order => order.Id == id);
+            _balance += removableOrder.CurentProfit;
+            _orders.Remove(removableOrder);
+        }
+
+
+        public void  Strategy()
+        {
+            var randomInt = new Random();
+
+            var randomOrderType = randomInt.Next(0, 2);
+
+            switch ((OrderType)randomOrderType)
+            {
+                case OrderType.BUY:
+
+                    break;
+
+                case OrderType.SELL:
+
+                    break;
+
+                default: break;
+            }
+        }
+
+    }
+
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public OrderType Type { get; set; }
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public decimal CurentProfit { get; set; }
+
+        public Order(string symbol, decimal price, OrderType type)
+        {
+            Id = Guid.NewGuid();
+            Symbol = symbol;
+            Price = price;
+            Type = type;
+        }
+    }
+
+    public enum OrderType
+    {
+        BUY = 1,
+        SELL = 2
+    } 
+}
